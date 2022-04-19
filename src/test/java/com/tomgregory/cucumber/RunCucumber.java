d8564ff61d1dab695cbc8738e6eba9b2413206e8
Vasudeva Annam <Vasudeva_Annam@trekbikes.com>
2022-04-19 13:27:10 -0400

    Generate single report from Multiple json files

--- src/test/java/com/tomgregory/cucumber/RunCucumber.java
+++ src/test/java/com/tomgregory/cucumber/RunCucumber.java
@@ -1,8 +1,15 @@
 package com.tomgregory.cucumber;
 
 import io.cucumber.core.cli.Main;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import net.masterthought.cucumber.presentation.PresentationMode;
+import net.masterthought.cucumber.sorting.SortingMethod;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -11,6 +18,8 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class RunCucumber {
+
+  private final static String REPORTS_PATH = "build/GridReports";
 
   public static void main(String[] args) throws ExecutionException, InterruptedException {
     List<List<String>> allExecutionArguments = getArguments(List.of(args));
@@ -21,6 +30,7 @@ public class RunCucumber {
     })).toList();
 
     List<Byte> exitStatuses = waitForCompletion(futures);
+    new GenerateReport().mergeReports();
     exitStatuses.stream().filter(exitStatus -> exitStatus != 0).findFirst().ifPresent(System::exit);
     System.exit(0);
   }
@@ -41,13 +51,50 @@ public class RunCucumber {
         argsCopy.set(args.indexOf("runtimeTag"), System.getProperty("tags"+i));
         int threadCount = StringUtils.isBlank(System.getProperty("threads"+i)) ? 1 : Integer.parseInt(System.getProperty("threads"+i));
         argsCopy.set(args.indexOf("runtimeThreads"), String.valueOf(threadCount));
-        argsCopy.set(args.indexOf("runtimeHtml"), "html:build/GridReports/Reports%s/cucumber-report.html".formatted(i));
-        argsCopy.set(args.indexOf("runtimeRerun"), "rerun:build/GridReports/Reports%s/rerun.txt".formatted(i));
-        argsCopy.set(args.indexOf("runtimeJson"), "json:build/GridReports/Reports%s/jsonReport.json".formatted(i));
+        argsCopy.set(args.indexOf("runtimeHtml"), "html:%s/Reports%s/cucumber-report.html".formatted(REPORTS_PATH, i));
+        argsCopy.set(args.indexOf("runtimeRerun"), "rerun:%s/Reports%s/rerun.txt".formatted(REPORTS_PATH, i));
+        argsCopy.set(args.indexOf("runtimeJson"), "json:%s/Reports%s/jsonReport.json".formatted(REPORTS_PATH, i));
         allExecutionArguments.add(new ArrayList<>(argsCopy));
       }
     }
     return allExecutionArguments;
+  }
+
+  private static final class GenerateReport {
+
+    public void mergeReports() {
+      {
+        try {
+          File reportOutputDirectory = new File(REPORTS_PATH);
+          List<String> jsonFiles = new ArrayList<>();
+          List<File> files = (List<File>) FileUtils.listFiles(reportOutputDirectory,
+                  new String[]{"json"}, true);
+          for (File file : files) {
+            jsonFiles.add(file.getCanonicalPath());
+          }
+
+          String buildNumber = "1";
+          String projectName = "Cucumber";
+          Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+          configuration.setBuildNumber(buildNumber);
+
+          configuration.addClassifications("Browser", "Chrome");
+          configuration.addClassifications("Branch", "master");
+          configuration.setSortingMethod(SortingMethod.NATURAL);
+          configuration.addPresentationModes(PresentationMode.EXPAND_ALL_STEPS);
+//							Files.createDirectories(Paths.get(projectPath + localReportsFolderName + "/JenkinsReport"));
+          configuration.setTrendsStatsFile(new File(REPORTS_PATH+"/jenkins.json"));
+          ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
+          reportBuilder.generateReports();
+          System.out.println("Local Jenkins Reports Generated");
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+
+
+    }
+
   }
 
 
